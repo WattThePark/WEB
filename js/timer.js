bdbc58cfb87534db81cfeda334438dd2fc44700d
AudeNL <aude.neel@gmail.com>
2016-01-07 09:43:12 +0100

    Modification PHP vers HTML + Ajout favicon

--- js/timer.js
+++ js/timer.js
@@ -3,6 +3,7 @@ var start = 0
 var end = 0
 var diff = 0
 var timerID = 0
+
 function chrono(){
 	end = new Date()
 	diff = end - start
@@ -31,16 +32,88 @@ function chrono(){
 	timerID = setTimeout("chrono()", 10)
 }
 function chronoStart(){
-	document.getElementById("startstop").innerHTML = "Arrêter"
+	
+	document.getElementById("startstop").innerHTML = "Arrêter le chrono"
 	document.getElementById("startstop").onclick = chronoStop
-	document.getElementById("reset").onclick = chronoReset
+	//document.getElementById("reset").onclick = chronoReset
 	start = new Date()
+	
+			var delay = ( function() {
+			var timer = 0;
+			return function(callback, ms) {
+				clearTimeout (timer);
+				timer = setTimeout(callback, ms);
+			};
+		})();
+
+	delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 90%,rgba(64,187,106,1) 90%)';
+		 document.getElementById("text").innerHTML="10 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 80%,rgba(64,187,106,1) 80%)';
+		document.getElementById("text").innerHTML="20 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 70%,rgba(64,187,106,1) 70%)';
+		document.getElementById("text").innerHTML="30 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 60%,rgba(64,187,106,1) 60%)';
+		document.getElementById("text").innerHTML="40 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 50%,rgba(64,187,106,1) 50%)';
+		document.getElementById("text").innerHTML="50 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 40%,rgba(64,187,106,1) 40%)';
+		document.getElementById("text").innerHTML="60 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 30%,rgba(64,187,106,1) 30%)';
+		document.getElementById("text").innerHTML="70 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 20%,rgba(64,187,106,1) 20%)';
+		document.getElementById("text").innerHTML="80 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 10%,rgba(64,187,106,1) 10%)';
+		document.getElementById("text").innerHTML="90 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("mydiv").style.background = 'linear-gradient(to bottom, white 0%,white 0%,rgba(64,187,106,1) 0%)';
+		document.getElementById("text").innerHTML="100 Watts";
+		delay(function(){
+		// do stuff
+		document.getElementById("image").src="./images/batterie-fin.png";
+		
+		document.getElementById("text").innerHTML="Vous avez généré 100 Watts <br/>";
+		}, 1000 );
+		}, 1000 );
+		}, 1000 );
+		}, 1000 );
+		}, 1000 );
+
+		}, 1000 );
+		}, 1000 );
+
+		}, 1000 );
+		}, 1000 );
+		}, 1000 );
+
+
+		}, 1000 );
+	
+
 	chrono()
 }
 function chronoContinue(){
-	document.getElementById("startstop").innerHTML = "Arrêter"
+	document.getElementById("startstop").innerHTML = "Arrêter le chrono"
 	document.getElementById("startstop").onclick = chronoStop
-	document.getElementById("reset").onclick = chronoReset
+	//document.getElementById("reset").onclick = chronoReset
 	start = new Date()-diff
 	start = new Date(start)
 	chrono()
@@ -54,8 +127,9 @@ function chronoStopReset(){
 	document.getElementById("startstop").onclick = chronoStart
 }
 function chronoStop(){
-	document.getElementById("startstop").innerHTML = "Lancer"
+	tmp = 1
+	document.getElementById("startstop").innerHTML = "Lancer le chrono"
 	document.getElementById("startstop").onclick = chronoContinue
-	document.getElementById("reset").onclick = chronoStopReset
+	//document.getElementById("reset").onclick = chronoStopReset
 	clearTimeout(timerID)
 }
